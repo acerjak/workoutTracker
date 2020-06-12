ffaf9749dd23f3741c704a2cbbe95c18e99ecf77
acerjak <62491401+acerjak@users.noreply.github.com>
2020-06-11 21:17:58 -0700

    redefined schema to fit secondary index

--- models/Exercise.js
+++ models/Exercise.js
@@ -2,12 +2,18 @@
 const { Schema, model } = require('mongoose')
 
 //define what exercise will look like for new data
-module.exports = model('Exercise', new Schema({
-    type: String,
-    name: String,
-    duration: Number,
-    weight: Number,
-    reps: Number,
-    sets: Number
+module.exports = model('Exercise', new Schema(
+{
+    // define first index
+    day: Date,
+        // define secondary index
+    exercise: [{
+        type: String, 
+        name: String,
+        duration: Number,
+        weight: Number,
+        reps: Number,
+        sets: Number
+    }]
 }))
 
