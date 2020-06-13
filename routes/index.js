d885166f86f799c84bde4764c0b61eed29f2472f
acerjak <62491401+acerjak@users.noreply.github.com>
2020-06-12 21:34:50 -0700

    updated index routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 //define exress router
 const router = require('express').Router()
-//require index routes
+//require api routes
 router.use('/api', require('./apiRoutes.js'))
+router.use('/api', require('./statsRoutes.js'))
 //export out router folder
 module.exports = router
